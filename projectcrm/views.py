b0a46de60928793a3a2d9e9872fe07b8b46a3f41
Mardoquel Gadi <mardocagadi@hotmail.com>
2023-04-10 22:59:52 -0300

    adicionando path de login e logout

--- projectcrm/views.py
+++ projectcrm/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html',{})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login feito com sucesso.')
+            return redirect('home')
+        else:
+            messages.success(request, 'Erro ao fazer login, tente novamente.')
+            return redirect('home')
+    else:
+        return render(request, 'home.html',{})
+
+def user_logout(request):
+    pass
