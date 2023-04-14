95436fb52ee12f6aa3c2a614251e9621cab731ed
Mardoquel Gadi <mardocagadi@hotmail.com>
2023-04-13 22:33:18 -0300

    adicionando path de registro de usuario

--- projectcrm/urls.py
+++ projectcrm/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
 ]
